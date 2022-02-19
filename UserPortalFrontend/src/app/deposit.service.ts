5d14be4ade8dfa83956c121dcfb3d8d63737d875
sayra <sayra.bano8888@gmail.com>
2022-02-20 05:16:01 +0530

    Adding frontend code

---
+++ UserPortalFrontend/src/app/deposit.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DepositService {
+  ROOT_URL: String = 'http://localhost:9090';
+
+  constructor(private http: HttpClient) {}
+
+  insertEntry(account: string, amount: number) {
+    var body = {
+      account: account,
+      amount: amount,
+    };
+    console.log(body);
+    return this.http.post(this.ROOT_URL + '/account/deposit', body);
+  }
+}
